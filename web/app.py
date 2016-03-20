99c1c64cc395ab82588d4a163ecb407630340a78
CJ <cjtheairelement@gmail.com>
2016-03-20 17:55:13 -0500

    wired in sqlalchemy to use psycopg2 api

--- web/app.py
+++ web/app.py
@@ -1,32 +1,68 @@
 from flask import Flask
-from psycopg2 import connect
-from subprocess import check_output
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+	UserMixin, RoleMixin, login_required
 
+DATABASE_HOST = 'db'
+DATABASE_PORT = 5432
+DATABASE_NAME = 'postgres'
+DATABASE_USER = 'admin'
+DATABASE_PASS = 'pass'
+
+CONNECTION_STR_FORMAT = "postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}"
+
+def connectionStr():
+	return str.format(
+		CONNECTION_STR_FORMAT,
+		DATABASE_USER,
+		DATABASE_PASS,
+		DATABASE_HOST,
+		DATABASE_PORT,
+		DATABASE_NAME
+	);
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = connectionStr()
 
-def getConn(dbname='db', dbhost = 'localhost', dbport=5432, dbuser='postgres', dbpass='postgres'):
-	connStr = str.format("dbname='{0}' host='{1}' user='{2}' password='{3}'", dbname, dbhost, dbuser, dbpass)
-	conn = connect(connStr)
-	return conn
+db = SQLAlchemy(app)
 
+# Define models
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    db.create_all()
+    user_datastore.create_user(email='cj@atmoscape.com', password='password')
+    db.session.commit()
+
+# Views
 @app.route('/')
-def hello():
-	returnStr = ''
-
-	try:
-		conn = getConn(dbname='postgres', dbhost='db', dbuser='admin', dbpass='pass')
-		returnStr += "connected"
-	except Exception, e:
-		return "Couldn't connect to database... %s" % str(e)
-
-	try:
-		cur = conn.cursor()
-		returnStr +=' got cursor'
-	except Exception, e:
-		return "Couldn't get cursor... %s" % str(e)
-
-	return "success"
+@login_required
+def home():
+    return "hello"
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8080, debug=True)
